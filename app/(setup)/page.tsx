15af0e01b1d2ded96ae0c392ff28fbca49e6f99e
An Le Dai <55825127+AnJuniors@users.noreply.github.com>
2023-10-22 02:41:03 +0700

    feat: imital modal ui (#2)

--- app/(setup)/page.tsx
+++ app/(setup)/page.tsx
@@ -2,6 +2,7 @@ import { redirect } from "next/navigation";
 
 import { db } from "@/lib/db";
 import { initialProfile } from "@/lib/initial-profile";
+import InitalModal from "@/components/modals/inital-modal";
 
 export default async function SetupPage() {
   const profile = await initialProfile();
@@ -18,5 +19,5 @@ export default async function SetupPage() {
 
   if (server) return redirect(`/server/${server.id}`);
 
-  return <div>Create a Server</div>;
+  return <InitalModal />;
 }
