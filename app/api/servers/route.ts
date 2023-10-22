6d156532abb3e06a075905a06157bc329ab13223
An Le Dai <55825127+AnJuniors@users.noreply.github.com>
2023-10-22 13:24:03 +0700

    navigation sidebar (#5)

--- app/api/servers/route.ts
+++ app/api/servers/route.ts
@@ -2,13 +2,13 @@ import { v4 } from "uuid";
 import { NextResponse } from "next/server";
 
 import { MemberRole } from "@prisma/client";
-import { CurrentProfile } from "@/lib/current-profile";
+import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
 
 export async function POST(req: Request) {
   try {
     const { name, imageUrl } = await req.json();
-    const profile = await CurrentProfile();
+    const profile = await currentProfile();
 
     if (!profile) return new NextResponse("unauthorized", { status: 401 });
 
